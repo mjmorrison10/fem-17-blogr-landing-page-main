ad2b5bf2cb411ff09ef3b0d55e6f629ae38cc5d7
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-11-05 11:58:19 -0700

    Drop down menu
    Bug  - hover effect applied to all 'li' items on drop-down-menu which is affecting the entire div.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 const product = document.querySelector(".product");
+const productButton = document.querySelector(".product-button");
+const company = document.querySelector(".company");
+const connect = document.querySelector(".connect");
+
+product.addEventListener("click", function () {
+  product.children[1].classList.toggle("show");
+  product.children[0].classList.toggle("menu-button");
+  console.log(product.children[1]);
+  console.log("product");
+});
+
+company.addEventListener("click", function () {
+  company.children[1].classList.toggle("show");
+  company.children[0].classList.toggle("menu-button");
+
+  console.log(company.children[1]);
+  console.log("company");
+});
+
+connect.addEventListener("click", function () {
+  connect.children[1].classList.toggle("show");
+  connect.children[0].classList.toggle("menu-button");
+
+  console.log(connect.children[1]);
+  console.log("connect");
+});
+
